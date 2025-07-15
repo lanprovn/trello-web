d5ce29f21c4318906edf14a3e2fc0c872b512117
lanprovn <lelanhoang1912@gmail.com>
2025-07-15 19:32:58 +0700

    Dark Light P1

--- src/theme.js
+++ src/theme.js
@@ -1,25 +1,23 @@
-import { createTheme } from '@mui/material/styles'
-import { red } from '@mui/material/colors'
-import { experimental_extendTheme as extendTheme} from '@mui/material/styles'
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
+import { teal, deepOrange, cyan, orange } from '@mui/material/colors'
 
 // Create a theme instance.
 const theme = extendTheme({
   colorSchemes: {
     light: {
       palette: {
-        // primary: {
-        //   main: '#ff5252'
-        // }
+        primary: teal,
+        secondary: deepOrange
       }
     },
     dark: {
       palette: {
-        // primary: {
-        //   main: '#000'
-        // }
+        primary: cyan,
+        secondary: orange
       }
     }
   }
+
 })
 
 export default theme
