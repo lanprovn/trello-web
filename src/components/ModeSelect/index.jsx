902fc609a767b3661c503b5f49ea612039a2c0f4
lanprovn <lelanhoang1912@gmail.com>
2025-07-22 10:10:11 +0700

    App bar p2

--- src/components/ModeSelect/index.jsx
+++ src/components/ModeSelect/index.jsx
@@ -6,6 +6,7 @@ import Select from '@mui/material/Select'
 import LightModeIcon from '@mui/icons-material/LightMode'
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
 import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+import Box from '@mui/material/Box'
 
 function ModeSelect() {
   const { mode, setMode } = useColorScheme()
@@ -16,7 +17,7 @@ function ModeSelect() {
   }
 
   return (
-    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+    <FormControl size="small">
       <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
       <Select
         labelId="label-select-dark-light-mode"
@@ -26,22 +27,22 @@ function ModeSelect() {
         onChange={handleChange}
       >
         <MenuItem value="light">
-          <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
-            <LightModeIcon fontSize='smaill' />
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <LightModeIcon fontSize='small' />
             Light
-          </div>
+          </Box>
         </MenuItem>
         <MenuItem value="dark">
-          <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
-            <DarkModeOutlinedIcon fontSize='smaill' />
+          <Box sx={{ display: 'flex', alignItems: 'center', gap:1 }}>
+            <DarkModeOutlinedIcon fontSize='small' />
             Dark
-          </div>
+          </Box>
         </MenuItem>
         <MenuItem value="system">
-          <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
-            <SettingsBrightnessIcon fontSize='smaill' />
+          <Box sx={{ display: 'flex', alignItems: 'center', gap:1 }}>
+            <SettingsBrightnessIcon fontSize='small' />
             System
-          </div>
+          </Box>
         </MenuItem>
       </Select>
     </FormControl>
